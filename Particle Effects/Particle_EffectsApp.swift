b551f6843e738062d8d3f8b4150d08a73d220e1b
justin747 <justin747@gmail.com>
2023-04-23 15:05:38 -0700

    Initial Commit

---
+++ Particle Effects/Particle_EffectsApp.swift
@@ -0,0 +1,17 @@
+//
+//  Particle_EffectsApp.swift
+//  Particle Effects
+//
+//  Created by Justin on 4/23/23.
+//
+
+import SwiftUI
+
+@main
+struct Particle_EffectsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
